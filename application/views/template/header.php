558f7bc77b1b1a8c5792852e0d5e99146d496a2c
Kashkas <crono.120@gmail.com>
2014-11-27 21:50:11 -0300

    Vista principal

--- application/views/template/header.php
+++ application/views/template/header.php
@@ -2,31 +2,21 @@
     <head>
         <title>Curso</title>
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
-        <script src="<?php echo base_url(); ?>jQuery/jquery-2.1.1.min.js"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script> 
+            <script src="<?php echo base_url(); ?>jQuery/jquery-2.1.1.min.js"></script>
+            <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script> 
     </head>
     <body>
-        <div class="container">
-            <div class="div0" style="vertical-align:top" >
-            <img src="<?php echo base_url('img/banner.jpg'); ?>"> 
-            <div class="div1" style="float:left">
-        <ul class="nav nav-list">
-            <li class="active">
-                <a href="#">Home</a>
-            </li>
-            <li>
-                <a href="#">Library</a>
-            </li>
-        </ul>
-
-        <ul id="sidebar" class="nav nav-pills nav-stacked" style="max-width: 200px;">
-            <li class="active"><a href="#"><span class="glyphicon glyphicon-off"></span>  Overview</a></li>
-            <li><a href="#"><span class="glyphicon glyphicon-user"></span>  Profile</a></li>
-            <li><a href="#"><span class="glyphicon glyphicon-lock"></span>  Access</a></li>
-            <li><a href="#"><span class="glyphicon glyphicon-envelope"></span>  Message</a></li>
-            <li><a href="#"><span class="glyphicon glyphicon-list"></span>  Schedule</a></li>
-            <li><a href="#"><span class="glyphicon glyphicon-signal"></span>  Statistics</a></li>
-            <li><a href="#"><span class="glyphicon glyphicon-comment"></span>  Chat</a></li>
-            <li><a href="#"><span class="glyphicon glyphicon-earphone"></span>  Contacts</a></li>
-        </ul>
+        <div class="container" style="width: 100%">
+            <div class="div0" style="clear: all; width: 100%" >
+                <img src="<?php echo base_url('img/banner.jpg'); ?>" style="width:100%; height: 40%"> 
+            </div>
+            <div class="div1" style="float:left; width:20%; height: 100%">
+                <ul id="sidebar" class="nav nav-pills nav-stacked" style="max-width: 200px;">
+                    <li class="active">
+                        <a href="<?php echo base_url(); ?>">Home</a>
+                    </li>
+                    <li>
+                        <a href="<?php echo base_url(); ?>">Externo</a>
+                    </li>
+                </ul>
             </div>
